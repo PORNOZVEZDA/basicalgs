172146d2f69b4583063933ad01920f81232df65f
Sergey Frolov <sergeyfrolov1995@yahoo.com>
2018-08-26 14:59:03 +0300

    added initial file for tiles generation

---
+++ country_house/beauty_geneerator.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+class Tile_color(Enum):
+    white = 'w'
+    red = 'r'
+    brown = 'b'
+    yellow = 'y'
+
+class Tile:
+    def __init__(self, m, n):
+        self.tile_matrix = [[None for i in range(n)] for i in range(m)]
+        self.last_empty_cell = (0, 0)
+        self.Y = m
+        self.X = n
+
+    def valid_coords(self, x, y):
+        if x >= self.X or y >= self.Y:
+            return False
+        if self.tile_matrix[y][x] != None:
+            return False
+        return True
+
+    def list_moves(self):
+        res = []
+        x, y = self.last_empty_cell
+        if valid_coords(x+1, y):
+            res += [[(x, y),(x+1, y)]]
+        if valid_coords(x, y+1):
+            res += [[(x,y),(x, y+1)]]
+        return res
+
+    def commit_move(self, x, y,x1, y1,color):
+        self.commit_change(x, y, x1, y1, color)
+
+    def uncommit_move(self, x, y, x1, y1):
+        self.commit_change(x, y, x1, y1, None)
+
+    def commit_change(self, x, y, x1, y1, color):
+        self.tile_matrix[x][y] = color
+        self.tile_matrix[x1][y1] = color
+
+
+
+if __name__ == '__main__':
+    desk = Tile(6, 6)
+
