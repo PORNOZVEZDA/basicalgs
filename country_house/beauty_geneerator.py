df613c964ed2463b87984f93eedac3f5c3039526
Sergey Frolov <sergeyfrolov1995@yahoo.com>
2018-08-26 20:38:17 +0300

    improved evaluation func

--- country_house/beauty_geneerator.py
+++ country_house/beauty_geneerator.py
@@ -10,9 +10,10 @@ class Tile:
     def __init__(self, m, n):
         self.tile_matrix = [[None for i in range(n)] for i in range(m)]
         self.last_empty_cell = (0, 0)
-        self.result = []
+        self.positions = []
         self.Y = m
         self.X = n
+        self.current_number = 0
 
     def valid_coords(self, x, y):
         if x >= self.X or y >= self.Y:
@@ -23,28 +24,59 @@ class Tile:
 
     #TODO create method that calculates next cell to fulfill
     #how to change lastx|lasty cell after commiting move?
-    def list_moves(self, lastx, lasty):
+    def list_moves(self):
         res = []
         x, y = self.last_empty_cell
-        if valid_coords(x+1, y):
+        if self.valid_coords(x+1, y):
             res += [[(x, y),(x+1, y)]]
-        if valid_coords(x, y+1):
+        if self.valid_coords(x, y+1):
             res += [[(x,y),(x, y+1)]]
         return res
 
-    def commit_move(self, x, y,x1, y1,color):
+    def commit_move(self, x, y, x1, y1,color):
         self.commit_change(x, y, x1, y1, color)
+        self.last_empty_cell = self.empty_cell()
+        self.current_number += 1
 
     def uncommit_move(self, x, y, x1, y1):
         self.commit_change(x, y, x1, y1, None)
+        self.last_empty_cell = min(x, x1), min(y, y1)
+        self.current_number -= 1
 
-    def commit_change(self, x, y, x1, y1, color):
-        self.tile_matrix[x][y] = color
-        self.tile_matrix[x1][y1] = color
+    def empty_cell(self):
+        for n,i in enumerate(self.tile_matrix):
+            if None in i:
+                return i.index(None), n
+        return(-1, -1)
+
+    def __str__(self):
+        res = ""
+        for i in self.tile_matrix:
+            cur = " ".join([str(j) for j in i]) + "\n"
+            res += cur
+        return res
+    def commit_change(self, x, y, x1, y1, tile):
+        self.tile_matrix[y][x] = tile
+        self.tile_matrix[y1][x1] = tile
 
     def evaluate(self, empty_cells):
         if empty_cells == 0:
+            #self.positions += [self.tile_matrix[:]]
+            print(self)
+            #print(self.last_empty_cell)
+            return
+        moves = self.list_moves()
+        for move in moves:
+            x,y = move[0][0],move[0][1]
+            x1,y1 = move[1][0],move[1][1]
 
+            self.commit_move(x,y,x1,y1,self.current_number)
+            self.evaluate(empty_cells-2)
+            self.uncommit_move(x,y,x1,y1)
+            
 if __name__ == '__main__':
-    desk = Tile(6, 6)
+    desk = Tile(4,4)
+    desk.evaluate(4*4)
+    desk.tile_matrix[2][2] = 6
+    print(desk.positions)
 
